cd11faae9fd7cd847601b48d73a1c278fee6b603
Atsushi.Iida <ef41614@gmail.com>
2018-06-15 17:21:39 +0900

    KagikkoAdv 2-13回目のコミット
    第三者視点カメラ導入。切り替えボタンも設置。

---
+++ Assets/Scripts/CamerasController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamerasController : MonoBehaviour {
+
+	public GameObject charamovemanager;
+	CharaMoveManager CharaMoveMscript;
+
+	public GameObject turnmanager;
+	TurnManager TurnMscript;
+
+	[SerializeField]
+	private GameObject MainCamera;   // インスペクターで主観カメラを紐づける
+	[SerializeField]
+	private GameObject UchanThirdPersonCamera;   // インスペクターで第三者視点カメラを紐づける
+	[SerializeField]
+	private GameObject PchanThirdPersonCamera; 
+
+	//☆################☆################  Start  ################☆################☆
+
+	void Start () {
+		CharaMoveMscript = charamovemanager.GetComponent<CharaMoveManager> ();
+		TurnMscript = turnmanager.GetComponent<TurnManager>(); 
+
+		UchanThirdPersonCamera.SetActive (false);
+		PchanThirdPersonCamera.SetActive (false);
+	}
+
+
+	//####################################  Update  ###################################
+
+	void Update () {
+		Debug.Log ("TurnMscript.canMove1P : "+TurnMscript.canMove1P);
+		Debug.Log ("TurnMscript.canMove2P : "+TurnMscript.canMove2P);
+	}
+
+	//####################################  other  ####################################
+
+	public void ChangeCamera(){
+		// カメラを切り替える
+		// ↓現在のactive状態から反転 
+		MainCamera.SetActive (!MainCamera.activeInHierarchy);
+		if(TurnMscript.canMove1P == true){
+			UchanThirdPersonCamera.SetActive (!UchanThirdPersonCamera.activeInHierarchy);
+//			PchanThirdPersonCamera.SetActive (!PchanThirdPersonCamera.activeInHierarchy);
+		}
+		if(TurnMscript.canMove2P == true){
+//			UchanThirdPersonCamera.SetActive (!UchanThirdPersonCamera.activeInHierarchy);
+			PchanThirdPersonCamera.SetActive (!PchanThirdPersonCamera.activeInHierarchy);
+		}
+	}
+
+	//#################################################################################
+
+}
+// End
